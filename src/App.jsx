2a8ba6dd2849f0b09bd70eb2cf602212b29c9349
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-01-05 11:36:25 +0700

    - create page + index

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-
+import { About } from './pages'
+About
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return <About />
 }
 
 export default App
