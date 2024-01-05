80d4b2abb32776dfd9424a80d42d04f39f532903
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-01-05 15:23:59 +0700

    - add error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
-import { useRouteError } from 'react-router-dom'
+import { useRouteError, Link } from 'react-router-dom'
 const Error = () => {
   const error = useRouteError()
   if (error.status === 404) {
-    return <h1>Not found</h1>
+    return (
+      <div class="hero min-h-screen bg-base-200">
+        <div class="hero-content text-center">
+          <div class="max-w-md">
+            <h1 class="text-5xl font-bold">Not Found</h1>
+            <p class="py-6">{error.data}</p>
+            <Link to="/" className="btn btn-primary">
+              Back to Homepage
+            </Link>
+          </div>
+        </div>
+      </div>
+    )
   } else {
-    return <div>Some Thing went Wrong</div>
+    return (
+      <div class="hero min-h-screen bg-base-200">
+        <div class="hero-content text-center">
+          <div class="max-w-md">
+            <h1 class="text-5xl font-bold">Some Thing went Wrong</h1>
+            <p class="py-6">{error.data}</p>
+            <Link to="/" className="btn btn-primary">
+              Back to Homepage
+            </Link>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
