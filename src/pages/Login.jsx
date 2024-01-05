cd524336697978959cc6af29f1a1b29144c76b41
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-01-05 16:05:45 +0700

    - form login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,42 @@
 import React from 'react'
-
+import { FormInput, SubmitBtn } from '../components'
+import { Form, Link } from 'react-router-dom'
 const Login = () => {
-  return <div>Login</div>
+  return (
+    <section className="h-screen grid place-items-center">
+      <form
+        method="post"
+        className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4"
+      >
+        <h4 className="text-center text-3xl font-bold">Login</h4>
+        <FormInput
+          type="email"
+          label="email"
+          name="identifier"
+          defaultValue="test@test.com"
+        />
+        <FormInput
+          type="password"
+          label="password"
+          name="password"
+          defaultValue="secret"
+        />
+        <div className="mt-4">
+          <SubmitBtn text="Login" />
+        </div>
+        <button className="btn btn-secondary btn-block">guest user</button>
+        <p className="text-center">
+          Not a member yet?{' '}
+          <Link
+            className="ml-2 link link-hover link-primary capitalize"
+            to="/register"
+          >
+            register
+          </Link>
+        </p>
+      </form>
+    </section>
+  )
 }
 
 export default Login
