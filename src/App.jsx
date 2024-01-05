ac8a0d44fea31dfc4ea5074dde28c10b25aec46f
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-01-05 12:07:07 +0700

    - init router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { About } from './pages'
-About
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import {
+  HomeLayout,
+  Login,
+  Register,
+  About,
+  Products,
+  Cart,
+  Checkout,
+  Orders,
+  Error,
+  Landing,
+  SingleProduct,
+} from './pages'
+
 function App() {
-  return <About />
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <HomeLayout />,
+      errorElement: <Error />,
+      children: [
+        {
+          index: true,
+          path: 'landing',
+          element: <Landing />,
+        },
+        {
+          index: true,
+          path: 'landing/:id',
+          element: <SingleProduct />,
+        },
+        {
+          path: 'about',
+          element: <About />,
+        },
+        {
+          path: 'products',
+          element: <Products />,
+        },
+        {
+          path: 'cart',
+          element: <Cart />,
+        },
+        {
+          path: 'checkout',
+          element: <Checkout />,
+        },
+        {
+          path: 'orders',
+          element: <Orders />,
+        },
+      ],
+    },
+    {
+      path: '/login',
+      element: <Login />,
+      errorElement: <Error />,
+    },
+    {
+      path: '/register',
+      element: <Register />,
+      errorElement: <Error />,
+    },
+  ])
+  return <RouterProvider router={router} />
 }
 
 export default App
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
-
+import { useRouteError } from 'react-router-dom'
 const Error = () => {
-  return <div>Error</div>
+  const error = useRouteError()
+  if (error.status === 404) {
+    return <h1>Not found</h1>
+  } else {
+    return <div>Some Thing went Wrong</div>
+  }
 }
 
 export default Error
