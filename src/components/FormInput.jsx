dbef67e70a994499fcdc68dea7d72887212f4021
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-01-05 15:46:51 +0700

    - add input + submit ele

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const FormInput = ({ label, name, type }) => {
+const FormInput = ({ label, name, type, defaultValue }) => {
   return (
     <label className="form-control w-full max-w-xs">
       <div className="label">
@@ -10,6 +10,7 @@ const FormInput = ({ label, name, type }) => {
         type={type}
         placeholder={label}
         name={name}
+        value={defaultValue}
         className="input input-bordered w-full max-w-xs"
       />
     </label>
---
+++ src/components/SubmitBtn.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useNavigation } from 'react-router-dom'
+const SubmitBtn = ({ text }) => {
+  const navigation = useNavigation()
+  const isSubmitting = navigation.state === 'submitting' ? true : false
+
+  return (
+    <button
+      className="btn btn-primary btn-block"
+      type="submit"
+      disabled={isSubmitting}
+    >
+      {isSubmitting && <span className="loading loading-spinner"></span>}
+      {isSubmitting ? 'sending...' : text || 'submit'}
+    </button>
+  )
+}
+
+export default SubmitBtn
