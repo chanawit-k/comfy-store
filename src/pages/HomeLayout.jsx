cdbe3f6311d61808f651484b505db116840ff1ad
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-01-06 09:21:24 +0700

    - add custom class

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 const HomeLayout = () => {
   return (
-    <div>
+    <div className="align-element">
       home page
       <Outlet />
     </div>
