82f7b165ae7c6debfb7bcadb46dd9d6db9ad1783
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-01-08 12:08:11 +0700

    - fix class to classname

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 const Header = () => {
   return (
-    <header class="bg-neutral py-2 text-neutral-content">
+    <header className="bg-neutral py-2 text-neutral-content">
       <div className="align-element flex justify-center sm:justify-end">
         <div className="flex gap-x-6 justify-center items-center">
           <Link to="/login" className="link link-hover text-xs sm:text-sm">
