338a37651395d061f29c77515c4e67141ba332da
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-01-08 10:45:43 +0700

    - create navbar

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import { Header } from '../components'
+import { Header, Navbar } from '../components'
 const HomeLayout = () => {
   return (
     <>
       <Header />
+      <Navbar />
       <div className="align-element">
         <Outlet />
       </div>
