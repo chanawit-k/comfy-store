17354a8b488cf8fb0cac3f184e23767ac4a214fe
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-01-09 11:59:45 +0700

    - add props size

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const FormInput = ({ label, name, type, defaultValue }) => {
+const FormInput = ({ label, name, type, defaultValue, size }) => {
   return (
     <label className="form-control w-full max-w-xs">
       <div className="label">
