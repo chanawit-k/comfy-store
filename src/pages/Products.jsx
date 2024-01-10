49b9e974237983b7ac6cdc8683573bb39beb65ad
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-01-10 12:00:17 +0700

    - add param

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { customFetch } from '../utils'
 import { Filters, ProductsContainer, PaginationContainer } from '../components'
-const url = 'products'
-export const loader = async () => {
-  const response = await customFetch(url)
+
+const url = '/products/'
+export const loader = async ({ request }) => {
+  const params = Object.fromEntries([
+    ...new URL(request.url).searchParams.entries(),
+  ])
+  const response = await customFetch(url, { params })
   const products = response.data
   return products
 }
