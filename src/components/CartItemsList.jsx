93be2748c75a58012225cddec957ec93abbc5c04
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-01-12 12:17:59 +0700

    - init cart

---
+++ src/components/CartItemsList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import CartTotals from './CartTotals'
+const CartItemsList = () => {
+  return (
+    <div className="lg:col-span-4 lg:pl-4">
+      <CartTotals />
+    </div>
+  )
+}
+
+export default CartItemsList
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
-
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import SectionTitle from '../components/SectionTitle'
+import { CartItemsList, CartTotals } from '../components'
 const Cart = () => {
-  return <div>Cart</div>
+  const [user, setUser] = useState(null)
+  const { numItemsInCart } = useSelector((store) => store.cart)
+  if (numItemsInCart === 0) {
+    return <SectionTitle text="Your cart is empty" />
+  }
+  return (
+    <>
+      <SectionTitle text="Shopping Cart" />
+      <div className="mt-8 grid gap-8  lg:grid-cols-12">
+        <div className="lg:col-span-8">
+          <CartItemsList />
+        </div>
+        <div className="lg:col-span-4 lg:pl-4">
+          <CartTotals />
+          {user ? (
+            <Link to="/checkout" className="btn btn-primary btn-block mt-8">
+              Proceed to checkout
+            </Link>
+          ) : (
+            <Link to="/login" className="btn btn-primary btn-block mt-8">
+              please login
+            </Link>
+          )}
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Cart
