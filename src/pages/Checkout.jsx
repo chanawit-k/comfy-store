118dd4105a7cfb5e262dff1c1b31b854e6aaae96
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-01-30 17:40:34 +0700

    - checkout page setup

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
-
+import { SectionTitle, CartTotals, CheckoutForm } from '../components'
+import { useSelector } from 'react-redux'
 const Checkout = () => {
-  return <div>Checkout</div>
+  const cartItems = useSelector((state) => state.cart.cartTotal)
+  if (cartItems.length === 0) {
+    return <SectionTitle text="Your cart is empty" />
+  }
+
+  return (
+    <>
+      <SectionTitle text={'Total'} />
+      <div className="mt-8 grid gap-8 md:grid-cols-2 items-start">
+        <CheckoutForm />
+        <CartTotals />
+      </div>
+    </>
+  )
 }
 
 export default Checkout
